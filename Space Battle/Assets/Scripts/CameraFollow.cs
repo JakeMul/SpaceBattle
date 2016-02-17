0a38d7ced30ee36cf5a7d17e8fafa8e56d5e87c6
Jake Mulholland <eckojay2192@gmail.com>
2016-02-17 18:58:39 +0000

    Initial push

---
+++ Space Battle/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public float smooth = 5.0f;
+    void Update()
+    {
+        transform.position = Vector3.Lerp(
+            transform.position, target.position,
+            Time.deltaTime * smooth);
+        transform.rotation = Quaternion.Lerp(
+            transform.rotation, target.rotation, 
+            Time.deltaTime * smooth);
+    }
+}
